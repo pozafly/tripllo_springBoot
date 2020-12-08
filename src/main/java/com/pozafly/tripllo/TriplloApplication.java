46c775f4761e27c4806444a75005bae19463c68b
HwangSunTae <pozafly@gmail.com>
2020-12-08 17:13:33 +0900

    init

---
+++ src/main/java/com/pozafly/tripllo/TriplloApplication.java
@@ -0,0 +1,13 @@
+package com.pozafly.tripllo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriplloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriplloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pozafly/tripllo/TriplloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pozafly.tripllo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriplloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
