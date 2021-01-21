b549b39e00c26772e167c99bc169774912dbf6a6
HwangSunTae <pozafly@gmail.com>
2021-01-21 10:57:21 +0900

    code deploy 다시

--- src/main/java/com/pozafly/tripllo/common/config/SwaggerConfig.java
+++ src/main/java/com/pozafly/tripllo/common/config/SwaggerConfig.java
@@ -25,7 +25,7 @@ public class SwaggerConfig {
 
     @Bean
     public Docket apiV1() {
-        version = "V1.1";
+        version = "V1.2";
         title = "Tripllo API " + version;
 
         List<ResponseMessage> responseMessages = new ArrayList<>();
