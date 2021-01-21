45a3a3ade735dfc361f84f0360e39a4a365b8360
HwangSunTae <pozafly@gmail.com>
2021-01-21 22:55:24 +0900

    test 어노테이션 제거

--- src/test/java/com/pozafly/tripllo/TriplloApplicationTests.java
+++ src/test/java/com/pozafly/tripllo/TriplloApplicationTests.java
@@ -1,12 +1,10 @@
 package com.pozafly.tripllo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TriplloApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
