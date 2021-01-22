7e05f4f801c070a7bdd7ce39ecf98768ccbc5f39
HwangSunTae <pozafly@gmail.com>
2021-01-22 14:26:05 +0900

    3번째 test

--- src/main/java/com/pozafly/tripllo/common/config/SwaggerConfig.java
+++ src/main/java/com/pozafly/tripllo/common/config/SwaggerConfig.java
@@ -25,7 +25,7 @@ public class SwaggerConfig {
 
     @Bean
     public Docket apiV1() {
-        version = "V1.4";
+        version = "V1.5";
         title = "Tripllo API " + version;
 
         List<ResponseMessage> responseMessages = new ArrayList<>();
