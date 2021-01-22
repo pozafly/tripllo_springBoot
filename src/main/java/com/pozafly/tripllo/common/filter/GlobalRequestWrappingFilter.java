1f984f5f1dc692380e21aae03460c5bc00f59efa
HwangSunTae <pozafly@gmail.com>
2021-01-22 18:50:40 +0900

    s3 파일 업로드 작업 전 라이브러리 정리

--- src/main/java/com/pozafly/tripllo/common/filter/GlobalRequestWrappingFilter.java
+++ src/main/java/com/pozafly/tripllo/common/filter/GlobalRequestWrappingFilter.java
@@ -1,16 +1,20 @@
 package com.pozafly.tripllo.common.filter;
 
-import org.apache.tomcat.util.json.JSONParser;
+import com.google.gson.Gson;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.LinkedHashMap;
 
 @Component
 @WebFilter(urlPatterns = "/api/**")
 public class GlobalRequestWrappingFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
@@ -18,11 +22,16 @@ public class GlobalRequestWrappingFilter implements Filter {
             System.out.println("--------------------filter--------------------");
             HttpServletRequest wrapper = new ReadableRequestWrapper((HttpServletRequest) request);
 
-            JSONParser parser = new JSONParser(((ReadableRequestWrapper) wrapper).getRequestBody());
-            wrapper.setAttribute("requestBody", parser.object());
+            Gson gson = new Gson();
+            LinkedHashMap requestBody = gson.fromJson(((ReadableRequestWrapper) wrapper).getRequestBody(), LinkedHashMap.class);
+
+            wrapper.setAttribute("requestBody", requestBody);
             chain.doFilter(wrapper, response);
         } catch (Exception e) {
             chain.doFilter(request, response);
         }
     }
+
+    @Override
+    public void destroy() {}
 }
