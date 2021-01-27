55b2a89f6b34318f94d8087209d6d6631164fde2
HwangSunTae <pozafly@gmail.com>
2021-01-27 22:17:41 +0900

    cors 설정

--- src/main/java/com/pozafly/tripllo/common/config/CorsConfig.java
+++ src/main/java/com/pozafly/tripllo/common/config/CorsConfig.java
@@ -12,6 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("http://tripllo-vue-build.s3-website.ap-northeast-2.amazonaws.com")
                 .allowedOrigins("*")
                 .allowedMethods(
                         HttpMethod.GET.name(),
