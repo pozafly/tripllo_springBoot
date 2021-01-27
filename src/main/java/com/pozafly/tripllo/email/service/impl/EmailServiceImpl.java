09273c76ae9ed81991166df9bb3cc3d734fe3b14
HwangSunTae <pozafly@gmail.com>
2021-01-27 10:46:49 +0900

    travis 테스트

--- src/main/java/com/pozafly/tripllo/email/service/impl/EmailServiceImpl.java
+++ src/main/java/com/pozafly/tripllo/email/service/impl/EmailServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
