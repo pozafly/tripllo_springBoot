3e5d8f7b571e2bac836e8632934dc1bb335f434c
HwangSunTae <pozafly@gmail.com>
2021-02-21 10:02:01 +0900

    스케줄러 막기

--- src/main/java/com/pozafly/tripllo/common/scheduler/ScheduleTask.java
+++ src/main/java/com/pozafly/tripllo/common/scheduler/ScheduleTask.java
@@ -14,17 +14,16 @@ public class ScheduleTask {
     @Autowired
     private TestUserJob testUserJob;
 
-//        @Scheduled(cron = "1-30/3 * * * * *", zone = "Asia/Seoul")
-    @Scheduled(cron = "0 0 7-23/2 * * *", zone = "Asia/Seoul")
-    public void excuteTask() {
-        log.info("스케줄러 시작 시간 : {}", new Date());
-
-        try {
-            testUserJob.excute();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-
-        log.info("스케줄러 완료");
-    }
+//    @Scheduled(cron = "0 0 7-23/2 * * *", zone = "Asia/Seoul")
+//    public void excuteTask() {
+//        log.info("스케줄러 시작 시간 : {}", new Date());
+//
+//        try {
+//            testUserJob.excute();
+//        } catch(Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        log.info("스케줄러 완료");
+//    }
 }
